c005053e2d2d2623fd3d998d707b117d12c728bb
Dominik Jungowski <Dominik@Jungowski.de>
2012-05-24 19:44:10 +0200

    erste version

--- gonzales.js
+++ gonzales.js
@@ -1,5 +1,8 @@
 var spdy = require('spdy'),
     fs = require('fs'),
+	imagesJs = require('./images.js'),
+	cssJs = require('./css.js'),
+	url = require('url');
 	keysDir = '/root/node_modules/spdy/keys';
 
 var options = {
@@ -9,8 +12,24 @@ var options = {
 };
 
 var server = spdy.createServer(options, function(req, res) {
-  res.writeHead(200);
-  res.end('hello world!');
+  var urlInfo = url.parse(req.url);
+
+  var imgRegex = /!(index\.html)$/;
+  if (urlInfo.pathname.search(imgRegex) && urlInfo.pathname != '/') {
+	res.writeHead(200);
+	try {
+		var resource = fs.readFileSync(__dirname + '/www.chip.de/' + urlInfo.path);
+	} catch (e) {
+		var resource = '';
+	}
+	res.end(resource);
+  }	else {
+	  res.writeHead(200);
+	  var content = fs.readFileSync(__dirname + '/www.chip.de/index.html');
+	  var css = cssJs.get(req, res, content);
+	  var images = imagesJs.get(req, res, content);
+	  res.end(content);
+  }
 });
 
 server.listen(443);
---
+++ images.js
@@ -0,0 +1,5 @@
+exports.get = function(res, content) {
+
+		
+
+}
